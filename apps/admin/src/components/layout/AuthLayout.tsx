6e9e390874016e0c3fb80a54571ea10dfecd6bcc
dslee0525 <lds8835@gmail.com>
2026-02-02 01:47:03 +0900

    feat:컬러 테마 변경 및 로그인 레이아웃 배경이미지 추가

--- apps/admin/src/components/layout/AuthLayout.tsx
+++ apps/admin/src/components/layout/AuthLayout.tsx
@@ -4,10 +4,10 @@ export default function AuthLayout() {
   return (
     <>
       <div className="flex h-screen w-full min-w-7xl overflow-hidden">
-        <div className="flex flex-col flex-1 bg-black"></div>
+        <div className="flex flex-col flex-1 bg-primary-light bg-[url('/images/bg_stack.png')] bg-no-repeat bg-cover bg-center"></div>
         <div className="flex flex-col flex-1 bg-white">
           <main className="flex-1 overflow-hidden relative">
-            <div className="flex min-h-screen  flex-col items-center justify-center">
+            <div className="flex min-h-screen flex-col items-center justify-center">
               <h2 className="text-3xl font-bold tracking-tight text-gray-900">
                 <span className="sr-only">Dada Lab</span>
                 <img
