fccd915d494b78ee162afda26d7257e8029b3da9
dslee0525 <lds8835@gmail.com>
2026-02-02 22:47:18 +0900

    feat:프리즈마 스키마 중에 프로젝트 role 필드 제거

---
+++ apps/api/prisma/migrations/20260202134329_remove_project_field_role/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `role` on the `Project` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Project" DROP COLUMN "role";
