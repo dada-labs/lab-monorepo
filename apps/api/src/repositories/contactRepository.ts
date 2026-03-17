0b6e13c79c16f40d9ae51734da8f332990e1d9c7
dslee0525 <lds8835@gmail.com>
2026-03-17 22:15:00 +0900

    feat:문의 생성 시, 첨부 파일 관련 백엔드 로직 추가 #3

--- apps/api/src/repositories/contactRepository.ts
+++ apps/api/src/repositories/contactRepository.ts
@@ -1,15 +1,10 @@
-import {
-  ContactStatus,
-  Visibility,
-  type Prisma,
-  type ProjectStatus,
-} from "@prisma/client";
+import { ContactStatus } from "@prisma/client";
 import prisma from "../config/prisma.js";
-import type { CreateContactDto } from "@shared";
+import type { CreateContactDto, FileBase } from "@shared";
 
 export const contactRepository = {
   // 생성
-  async create(data: CreateContactDto) {
+  async create(data: CreateContactDto, attachments?: FileBase[]) {
     return await prisma.contact.create({
       data: {
         name: data.name,
@@ -17,6 +12,18 @@ export const contactRepository = {
         title: data.title,
         content: data.content,
         status: ContactStatus.UNREAD,
+        // 첨부파일
+        ...(attachments &&
+          attachments.length > 0 && {
+            attachments: {
+              create: attachments.map((attr) => ({
+                file: { create: attr },
+              })),
+            },
+          }),
+      },
+      include: {
+        attachments: { include: { file: true } },
       },
     });
   },
--- apps/api/src/services/contactService.ts
+++ apps/api/src/services/contactService.ts
@@ -1,17 +1,31 @@
+import { uploadToCloudinary } from "src/utils/cloudinary.js";
 import { contactRepository } from "../repositories/contactRepository.js";
 import type { CreateContactDto } from "@shared";
 
 export class ContactService {
   async createContact(
     data: CreateContactDto,
-    files: { docs?: Express.Multer.File[] }
+    files?: { docs?: Express.Multer.File[] }
   ) {
     if (!data.content) {
       throw new Error("문의 내용은 필수 항목입니다.");
     }
 
     // 파일 업로드 로직 필요
+    const attachments = [];
+    if (files?.docs) {
+      for (const file of files.docs) {
+        const res: any = await uploadToCloudinary(file, "attachments");
+        attachments.push({
+          url: res.secure_url,
+          key: res.public_id,
+          fileName: file.originalname,
+          fileType: file.mimetype,
+          fileSize: res.bytes,
+        });
+      }
+    }
 
-    return await contactRepository.create(data);
+    return await contactRepository.create(data, attachments);
   }
 }
